641b5d496cd737b6f1d92cc19452353ee80945a2
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-10 03:03:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,7 +43,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-    move(board, index, current_player)
+    move(board, index)
   else
     turn
   end
