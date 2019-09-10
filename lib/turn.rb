f1f58c2687829532bdb06064cd5516bb4e95f7f6
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-10 02:44:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,25 +10,18 @@ end
 # end of display board
 
 def valid_move?(board, index)
-  position_taken?(board, index)
-  if position_taken?(board, index) == false
-    board[index].between?("0", "8")
-    move = true
-  elsif board[index] < "0" || > "8"
-    move = false
+  if index.between?(1,9)
+    if !position_taken?(board, index)
+      true
+    end
   end
-  move
+
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  index_number = board[index]
-  if index_number == " " || index_number == "" || index_number == false
-    taken = false
-  elsif index_number == "X" || index_number == "O"
-    taken = true
-  end
-  taken
+  board[index] != " "
 end
 # end of position_taken
 
